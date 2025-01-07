7cc9ad2e5d0e2768f00bebf452044895bac72c8b
Daniel Yacobi <danieljoshua.yacobi@post.runi.ac.il>
2025-01-07 10:36:49 +0200

    user check

--- User.java
+++ User.java
@@ -44,7 +44,7 @@
     /** If this user follows the given name, returns true; otherwise returns false. */
     public boolean follows(String name) {
         for (int i = 0; i < fCount; i++) {
-            if (follows[i].equals(name)) {
+            if (follows[i].equals(name) && follows[i] != null) {
                 return true;
             }
         }
@@ -53,7 +53,7 @@
     /** Makes this user follow the given name. If successful, returns true. 
      *  If this user already follows the given name, or if the follows list is full, does nothing and returns false; */
     public boolean addFollowee(String name) {
-        if (fCount > 9 || follows(name)) {
+        if (fCount >= 10 || follows(name)) {
             return false;
         }
         this.follows[fCount] = name;
